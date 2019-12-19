347dddee694c6be11a0159befe517934fb044b0e
Darrin Lowery <lambda@monsto.com>
2019-12-18 22:10:45 -0600

    navbar scaffold

--- celebdoa/src/components/Navbar.js
+++ celebdoa/src/components/Navbar.js
@@ -4,26 +4,19 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Login from "./Login";
 import Register from "./Register";
 
-import { Navbar as NB, Nav, } from 'react-bootstrap';
+import { Navbar as NavbarBS, Nav, } from 'react-bootstrap';
 
 const Navbar = () => {
 	return (
 		<Router>
-			<NB bg="dark">
-				<ul>
-					<li><Link to="/register">Register</Link></li>
-					<li><Link to="/login">Login</Link></li>
-				</ul>
-			</NB>
+			<NavbarBS bg="dark">
+			<NavbarBS.Brand href="#home">React-Bootstrap</NavbarBS.Brand>
+				<Nav.Link><Link to="/register">Register</Link></Nav.Link>
+				<Nav.Link><Link to="/login">Login</Link></Nav.Link>
+			</NavbarBS>
 			<Route path="/login" component={Login} />
 			<Route path="/register" component={Register} />
 		</Router>
-
-			// {/* <ul>
-			// 		<li><a href="#">Register</a></li>
-			// 		<li><a href="#">Login</a></li>
-			// 		<li><a href="#">Play DOA!</a></li>
-			// 	</ul> */}
 	);
 };
 
