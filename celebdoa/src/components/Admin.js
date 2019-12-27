99bc45ecab8326614810786768d4c310c15cb66d
Darrin Lowery <lambda@monsto.com>
2019-12-27 11:50:51 -0600

    figuring out admin page "Alive?" radio button.

--- celebdoa/src/components/Admin.js
+++ celebdoa/src/components/Admin.js
@@ -1,47 +1,74 @@
-import React, { useState } from 'react';
+import React  from 'react';
+import { Button, Card, InputGroup, FormControl } from "react-bootstrap";
 
-import {
-	ButtonGroup,
-	ToggleButton,
-	InputGroup,
-	FormControl
-} from 'react-bootstrap';
+import { useInputControl } from "./hooks/useInputControl.js";
 
-
-function clg(...x) { console.log(...x) }  // because i'm sick of mistyping console.log
+function clg(...x) { console.log(...x); } // because i"m sick of mistyping console.log
 
 const Login = (props) => {
-	const [doaFields, setDoaFields] = useState({ celebName: "", factoid1: "", factoidA: "", dob: "", imageurl: "", doa: "" });
+	// #region upper component 
+	const celebNameInput = useInputControl("");
+	const factoidInput = useInputControl("");
+	const imageUrlInput = useInputControl("");
+	const aliveInput = useInputControl("");
+	const dobInput = useInputControl("");
 
-	// form field 
-	const doFields = e => {
-		e.preventDefault();
-		setDoaFields({ ...doaFields, [e.target.name]: e.target.value });
-		clg(doaFields);
+
+	const chkAliveField = () => {
+		if (aliveInput.value.length > 1) {
+			aliveInput.value = aliveInput.value.charAt(aliveInput.value.length - 1);
+			clg(18, aliveInput)
+		}
+		return aliveInput.value
 	}
 
-	const handleSubmit = e => {
+	const celebInfo = {
+		celebName: celebNameInput.value,
+		factoid: factoidInput.value,
+		dob: dobInput.value,
+		imageUrl: imageUrlInput.value,
+		alive: aliveInput.value
+	};
+
+	const doSubmit = e => {
 		e.preventDefault();
 
 		/* from Darrin for Darrin
 		validate the length of the fields
 		if field.length < 4, display an error. */
 
-		clg("admin page submitted")
+		clg("admin page submitted", celebInfo)
 	}
+	// #endregion
 
 	return (
-		<div>
-			<h1>Login</h1>
-			<form onSubmit={handleSubmit}>
-				<input type='text' name='celebName' value={doaFields.celebName} placeholder="Name of celebrity (required)" onChange={doFields}></input>
-				<input type='text' name='factoid' value={doaFields.factoid} placeholder="Short factoid (required)" onChange={doFields}></input>
-				<input type='text' name='dob' value={doaFields.dob} placeholder="Date of birth" onChange={doFields}></input>
-				<input type='text' name='imageurl' value={doaFields.imageurl} placeholder="URL to image (required)" onChange={doFields}></input>
-				<input type='text' name='doa' value={doaFields.doa} placeholder="URL to image (required)" onChange={doFields}></input>
-				<button>Login</button>
+		<Card style={{ width: '40rem' }}>
+			<form onSubmit={doSubmit}>
+				<Card.Header>
+					<Card.Title bg="light">Add Celeb</Card.Title>
+				</Card.Header>
+				<Card.Body style={{ padding: "2rem" }}>
+					<InputGroup className="mb-3">
+						<FormControl style={{ minWidth: "45%" }} {...celebNameInput} placeholder="Celebrity" />
+						<FormControl {...dobInput} placeholder="Birth Year" />
+						<FormControl
+							onChange={aliveInput.onChange}
+							value={chkAliveField()}
+							placeholder="Alive? Put Y or N."
+						/>
+					</InputGroup>
+					<InputGroup className="mb-3">
+						<FormControl {...imageUrlInput} placeholder="Image URL" />
+					</InputGroup>
+					<InputGroup className="mb-3">
+						<FormControl {...factoidInput} placeholder="Factoid" />
+					</InputGroup>
+					<Button variant="primary" type="submit" style={{ width: "10rem" }}>
+						Add Celeb
+				</Button>
+				</Card.Body>
 			</form>
-		</div>
+		</Card>
 	);
 }
 
