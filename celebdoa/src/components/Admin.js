1e2982f3d5f8bf5809259efbc6ce1df0f1aa657d
Darrin Lowery <lambda@monsto.com>
2019-12-27 13:39:40 -0600

    Cleanup.

--- celebdoa/src/components/Admin.js
+++ celebdoa/src/components/Admin.js
@@ -1,27 +1,17 @@
 import React, { useState } from 'react';
-import { Button, Card, InputGroup, FormControl, ToggleButton, ToggleButtonGroup, ButtonGroup } from "react-bootstrap";
+import { Button, Card, InputGroup, FormControl, ToggleButton, ToggleButtonGroup } from "react-bootstrap";
 
 import { useInputControl } from "./hooks/useInputControl.js";
 
 function clg(...x) { console.log(...x); } // because i"m sick of mistyping console.log
 
-const Login = (props) => {
+const CelebAdmin = (props) => {
 	const [alive, setAlive] = useState(true)
 
-	// #region upper component 
 	const celebNameInput = useInputControl("");
 	const factoidInput = useInputControl("");
 	const imageUrlInput = useInputControl("");
 	const dobInput = useInputControl("");
-
-
-	// const chkAliveField = () => {
-	// 	if (aliveInput.value.length > 1) {
-	// 		aliveInput.value = aliveInput.value.charAt(aliveInput.value.length - 1);
-	// 		clg(18, aliveInput)
-	// 	}
-	// 	return aliveInput.value
-	// }
 
 	const celebInfo = {
 		celebName: celebNameInput.value,
@@ -34,18 +24,13 @@ const Login = (props) => {
 	const doSubmit = e => {
 		e.preventDefault();
 
-		/* from Darrin for Darrin
-		validate the length of the fields
-		if field.length < 4, display an error. */
-
 		clg("admin page submitted", celebInfo)
 	}
-	// #endregion
 
 	const doAlive = e => {
-		// const chgAlive = e.target.value;
-		// clg(47, alive, chgAlive);
-		// // setAlive({chgAlive});
+		const chgAlive = e.target.value;
+		clg(47, chgAlive);
+		setAlive(chgAlive);
 	};
 
 	return (
@@ -59,16 +44,13 @@ const Login = (props) => {
 						<FormControl style={{ minWidth: "50%" }} {...celebNameInput} placeholder="Celebrity" />
 						<FormControl {...dobInput} placeholder="Birth Year" style={{ maxWidth: "25%" }} />
 						<ToggleButtonGroup name="alivequestion" defaultValue={true}>
-							<ToggleButton type="radio" name="alive" value={true} checked={alive === true} onClick={() => setAlive(true)} onChange={doAlive} variant="outline-primary" >
+							<ToggleButton type="radio" name="alive" value={true} checked={alive === true} onChange={doAlive} variant="outline-primary" >
 								Alive
 							</ToggleButton>
-							<ToggleButton type="radio" name="dead" value={false} checked={alive === false} onClick={() => setAlive(false)} onChange={doAlive} variant="outline-primary">
+							<ToggleButton type="radio" name="dead" value={false} checked={alive === false} onChange={doAlive} variant="outline-primary">
 								Dead
 							</ToggleButton>
 						</ToggleButtonGroup>
-						{/* <input value={false} checked={alive === false} onClick={() => setAlive(false)} type="radio" onChange={doAlive} />
-						<input value={true} checked={alive === true} onClick={() => setAlive(true)} type="radio" onChange={doAlive} />
-						<input value={false} checked={alive === false} onClick={() => setAlive(false)} type="radio" onChange={doAlive} /> */}
 					</InputGroup>
 					<InputGroup className="mb-3">
 						<FormControl {...imageUrlInput} placeholder="Image URL" />
@@ -85,4 +67,4 @@ const Login = (props) => {
 	);
 }
 
-export default Login;
+export default CelebAdmin;
