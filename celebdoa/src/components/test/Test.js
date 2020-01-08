b98c54a544303d2d55b35efe7dd22fea0667c9d5
Chris <caquinn.86@gmail.com>
2020-01-08 14:56:14 -0800

    Did more work on the handling of the questions and card rendering

--- celebdoa/src/components/test/Test.js
+++ celebdoa/src/components/test/Test.js
@@ -1,48 +1,66 @@
 import React from 'react';
 import axios from 'axios';
 
+import TestCard from './TestCard';
+import TestComplete from './TestComplete';
+
+const initialList = [
+    {
+        name: "Celeb One",
+        status: "Dead"
+    },
+    {
+        name: "Celeb Two",
+        status: "Alive"
+    },
+    {
+        name: "Celeb Three",
+        status: "Dead"
+    },
+    {
+        name: "Celeb Four",
+        status: "Alive"
+    }
+]
+
 class Test extends React.Component {
-    // utilize state to hold history of test scores
     state = {
-        pastScores: [],
-        celebList: [
-            {
-                name: "CelebOne",
-                alive: false
-            },
-            {
-                name: "CelebTwo",
-                alive: true
-            }
-        ],
-        celebPosition: 0
+        celebPosition: 0,
+        score: 0,
+        celeb: initialList[0],
+        finished: false
     }
 
-    score = 0;
+    componentDidMount() {
+        // Perform axios request and set the list variable to it
+        // Assign the first element in the list array to celeb
+    }
 
     handleAnswer = answer => {
-        let alive = this.state.celebList.celebs[celebPosition].alive;
-        answer == alive ? score++ : null;
-        this.state.celebPosition++;
+        if(!this.state.finished){
+            if(answer == this.state.celeb.status) {
+                this.state.score++;
+            }
+            this.state.celebPosition++;
+            this.state.celeb = initialList[this.state.celebPosition];
+            if(this.state.celebPosition >= initialList.length) {
+                this.state.finished = true;
+            }
+        }
+        this.forceUpdate();
     }
 
     render() {
         return(
             <>
-                <h2>{this.state.celebList.celebs[celebPosition].name}</h2>
-                <p>{this.state.celebList.celebs[celebPosition].dead}</p>
-                <span>
-                    <button onClick={handleAnswer(false)}>Dead</button>
-                    <button onClick={handleAnswer(true)}>Alive</button>
-                </span>
+                {this.state.finished
+                    ? (<TestComplete score={this.state.score} questions={initialList.length}/>)
+                    : (<TestCard celeb={this.state.celeb} answer={this.handleAnswer}/>)
+                }
+                
             </>
         );
     }
-    // display scoreboard for right and wrong answers
-    // get a list of celebrities
-    // display name
-    // display picture
-    // display two buttons for Alive and Dead
 }
 
 export default Test;
